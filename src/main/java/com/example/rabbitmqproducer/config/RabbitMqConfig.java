aea59e79f01341a7e49a4759683bf08dfe27abff
Sonali Mendis <smendis@scottlogic.com>
2023-04-21 21:45:08 +0100

    remove unneccessory code

--- src/main/java/com/example/rabbitmqproducer/config/RabbitMqConfig.java
+++ src/main/java/com/example/rabbitmqproducer/config/RabbitMqConfig.java
@@ -21,41 +21,6 @@ public class RabbitMqConfig {
         return new DirectExchange(properties.getDirectExchange(), true, false);
     }
 
-//    @Bean
-//    public Queue requestQueue(){
-//        return QueueBuilder
-//                .durable(properties.getDirectQueue())
-//                .withArgument("x-dead-letter-exchange", properties.getDirectQueue())
-//                .withArgument("x-dead-letter-routing-key", properties.getDirectDeadLetterQueue())
-//                .build();
-//    }
-//
-//    @Bean
-//    public Binding bindRequestQueue(){
-//        return BindingBuilder
-//                .bind(requestQueue())
-//                .to(directExchange())
-//                .with(properties.getDirectQueue())
-//                .noargs();
-//    }
-//
-//    @Bean
-//    public Queue requestDLQueue(){
-//        return QueueBuilder
-//                .durable(properties.getDirectDeadLetterQueue())
-//                .build();
-//    }
-//
-//    @Bean
-//    public Binding bindRequestDLQueue(){
-//        return BindingBuilder
-//                .bind(requestDLQueue())
-//                .to(directExchange())
-//                .with(properties.getDirectDeadLetterQueue())
-//                .noargs();
-//    }
-
-//    @Bean
     Jackson2JsonMessageConverter messageConverter(ObjectMapper mapper){
         var converter = new Jackson2JsonMessageConverter(mapper);
         converter.setCreateMessageIds(true); //create a unique message id for every message
